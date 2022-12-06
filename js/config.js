2b6b9a115253c159f5b172ead0d4c92ece485dd6
Geonovum <>
2022-12-06 12:01:30 +0000

    Update from https://github.com/Geonovum/word2markdown/commit/75c834d3444f03caf034cc79872091b9a75d2014

---
+++ js/config.js
@@ -0,0 +1,12 @@
+var respecConfig =
+{
+  pubDomain: "Geen",
+  specStatus: "Geen",
+  specType: "Geen",
+  license: "Geen",
+  publishDate: "2022-12-06",
+  format: "markdown",
+  edDraftURI: "https://Geonovum.github.io/Geen-/",
+  publishISODate: "2022-12-06T00:00:00.000Z",
+  generatedSubtitle: " 6 december 2022"
+}
