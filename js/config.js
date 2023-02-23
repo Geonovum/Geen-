acb0bab375d9ef44dda4fb21663d33f44ecf5883
Geonovum <>
2023-02-23 10:36:39 +0000

    Update from https://github.com/Geonovum/word2werkversie/commit/1a219de06ee3f5d10a9357fd334006dbc41324d2

--- js/config.js
+++ js/config.js
@@ -4,9 +4,11 @@ var respecConfig =
   specStatus: "Geen",
   specType: "Geen",
   license: "Geen",
-  publishDate: "2022-12-06",
+  publishDate: "2023-02-23",
+  content: "",
+  shortName: "",
   format: "markdown",
   edDraftURI: "https://Geonovum.github.io/Geen-/",
-  publishISODate: "2022-12-06T00:00:00.000Z",
-  generatedSubtitle: " 6 december 2022"
+  publishISODate: "2023-02-23T00:00:00.000Z",
+  generatedSubtitle: " 23 februari 2023"
 }
